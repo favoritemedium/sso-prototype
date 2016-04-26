c9a524d5c386ddb613908ee85c44e43a3a7a53b9
Hu Ziming <hzmangel@gmail.com>
2016-04-26 15:25:53 +0800

    Add github app conf.
    This is just a dev version so put id/secret in code directly. This
    should be REMOVED in prod version.

--- sso/apps.py
+++ sso/apps.py
@@ -3,3 +3,5 @@ from django.apps import AppConfig
 
 class SsoConfig(AppConfig):
     name = 'sso'
+    github_client_id = '844189c44c56ff04e727'
+    github_client_secret = '0bfecee7a78ee0e800b6bff85b08c140b91be4cc'
