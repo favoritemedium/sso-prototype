54d67ce544e95ecb58a62062ffe50fcd95db6f09
Hu Ziming <hzmangel@gmail.com>
2016-04-26 20:40:47 +0800

    Load github config from external file

--- sso/apps.py
+++ sso/apps.py
@@ -1,7 +1,14 @@
+import json
+import os.path
 from django.apps import AppConfig
+from fmproject import settings
 
 
 class SsoConfig(AppConfig):
+    base_config = json.load(
+        open(os.path.join(settings.BASE_DIR, 'fmproject', 'config.json'))
+    )
     name = 'sso'
-    github_client_id = '844189c44c56ff04e727'
-    github_client_secret = '0bfecee7a78ee0e800b6bff85b08c140b91be4cc'
+    github_client_id = base_config['github']['client_id']
+    github_client_secret = base_config['github']['client_secret']
+
