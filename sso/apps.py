daf5ffb9dcf93d38ef9aa5d64c1dd535ceb66ef9
Hu Ziming <hzmangel@gmail.com>
2016-05-04 13:56:01 +0800

    Wrapper for app ids and secrets

--- sso/apps.py
+++ sso/apps.py
@@ -9,6 +9,13 @@ class SsoConfig(AppConfig):
         open(os.path.join(settings.BASE_DIR, 'fmproject', 'config.json'))
     )
     name = 'sso'
+
     github_client_id = base_config['github']['client_id']
     github_client_secret = base_config['github']['client_secret']
+
+    google_client_id = base_config['google']['client_id']
+    google_client_secret = base_config['google']['client_secret']
+
+    facebook_client_id = base_config['facebook']['client_id']
+    facebook_client_secret = base_config['facebook']['client_secret']
 
